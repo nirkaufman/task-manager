5f7365f02ba4bea34817a9e200375e76559522f3
nirkaufman <nirkaufman@gmail.com>
2019-06-15 17:49:33 +0200

    step 3: task entity, spec and tasks controller

---
+++ src/tasks/task.entity.ts
@@ -0,0 +1,8 @@
+export class Task {
+    id: number;
+    title: string;
+    description: string;
+    created_at: string;
+    creator_id: number;
+    assigned_to: number;
+}
--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,10 +1,31 @@
-import {Controller, Get} from '@nestjs/common';
+import {Body, Controller, Delete, Get, Param, Post, Put} from '@nestjs/common';
+import {Task} from './task.entity';
 
 @Controller('tasks')
 export class TasksController {
 
     @Get()
-    ping(): string {
-        return "Tasks controller alive!"
+    allTasks(): Task[] {
+        return [];
+    }
+
+    @Get(':id')
+    getTaskById(@Param('id') id: number): Task {
+        return <Task>{id};
+    }
+
+    @Post()
+    createNewTask(@Body() newTask: Partial<Task>): any{
+        return "new task created successful";
+    }
+
+    @Put(':id')
+    updateTaskById(@Param('id') id: number): any {
+        return `task # ${id} updated`;
+    }
+
+    @Delete(':id')
+    deleteTaskById(@Param('id') id: number): any {
+        return `task # ${id} deleted`;
     }
 }
