2239896ef66d3092f9f3a1c9d2a1fb13a5c15347
nirkaufman <nirkaufman@gmail.com>
2019-06-20 06:13:29 -0400

    swagger

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import {LogInterceptor} from './log.interceptor';
+import {DocumentBuilder, SwaggerModule} from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalInterceptors(new LogInterceptor());
+
+  const options = new DocumentBuilder()
+      .setTitle('Tasks Manager App')
+      .setDescription('The Task manager API description')
+      .setVersion('1.0')
+      .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
